7412fa5e5840e9f48d2ff4e4904bfefc3a012761
Fittipaldih <fittipaldi.h@gmail.com>
2023-05-20 20:25:41 -0300

    add view lobby y ranking

---
+++ view/lobby.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lobby</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+<div class="container mt-5">
+    <h1 class="text-center display-1">¡Bienvenido Fitti!</h1>
+    <h3 class="text-center">Tu puntaje actual: 132</h3>
+    <div class="d-grid gap-2 col-6 mx-auto mt-4">
+        <button class="btn btn-primary btn-lg">Nueva partida</button>
+        <a href="ranking.php" class="btn btn-outline-primary btn-lg">Ver clasificación</a>
+    </div>
+    <div class="mt-5 mx-auto">
+        <h3>Partidas jugadas:</h3>
+            <table class="table table-striped ">
+                <thead>
+                <tr>
+                    <th>Partida:</th>
+                    <th>Contra:</th>
+                    <th>Resultado:</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Partida 1</td>
+                    <td>Jugador 1</td>
+                    <td>Resultado 1</td>
+                </tr>
+                <tr>
+                    <td>Partida 2</td>
+                    <td>Jugador 3</td>
+                    <td>Resultado 2</td>
+                </tr>
+                <tr>
+                    <td>Partida 3</td>
+                    <td>Jugador 1</td>
+                    <td>Resultado 3</td>
+                </tr>
+                </tbody>
+            </table>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
+
---
+++ view/ranking.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ranking</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+<main>
+    <br>
+    <div class="container">
+        <h2 class="text-center display-3"">Clasificación & Puntajes</h2>
+        <br>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th class="col-6">Jugador</th>
+                <th class="col-5">Puntaje</th>
+                <th class="col-1">#</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td class="col-6">Mar</td>
+                <td class="col-5">150</td>
+                <td class="col-1">1°</td>
+            </tr>
+            <tr>
+                <td class="col-6">Fitti</td>
+                <td class="col-5">132</td>
+                <td class="col-1">2°</td>
+            </tr>
+            </tbody>
+        </table>
+        <a href="lobby.php" class="btn btn-outline-primary btn-lg">Volver al lobby</a>
+    </div>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
+
